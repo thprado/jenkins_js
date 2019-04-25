eb2cde4bece60d09b798ab3a5538df26f2a99335
Thiago Prado <thiago.henrique.prado@everis.com>
2019-04-25 16:18:52 -0300

    Only function to edit div multibranch pipeline

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 var everJs = jQuery.noConflict();
 
-if (everJs("#description").next().find("h1").length > 0){
-	if (everJs("#description").next().find("h1").text() === "This folder is empty") {
-		everJs("#description").next().find("div").addClass('noBackground');
+function editMultiBranchPipelineDiv() {
+	if (everJs("#description").next().find("h1").length > 0){
+		if (everJs("#description").next().find("h1").text() === "This folder is empty") {
+			everJs("#description").next().find("div").addClass('noBackground');
+		}
 	}
 }
 
+// everJs(document).ready(function(){
+// 	if (everJs("#description").next().find("h1").length > 0){
+// 		if (everJs("#description").next().find("h1").text() === "This folder is empty") {
+// 			everJs("#description").next().find("div").addClass('noBackground');
+// 		}
+// 	}
+// });
