33de6d97e11f8bdbd78c17fd2b2f296b060cb1d3
Thiago Prado <thiago.henrique.prado@everis.com>
2019-04-25 11:27:40 -0300

    Detect the div without class in multibranch pipeline and insert a class

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 var everJs = jQuery.noConflict();
 
+if (everJs("#description").next().find("h1").length > 0){
+	if (everJs("#description").next().find("h1").text() === "This folder is empty") {
+		everJs("#description").next().find("div").addClass('noBackground');
+	}
+}
+
